7703f382428faba226bfe4c07e99904ba6d96e5d
koala320 <koala_320@126.com>
2023-07-24 18:06:09 +0800

    项目初始化-tabbar

---
+++ src/components/tab-bar/tab-bar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="tab-bar">
+    <van-tabbar v-model="currentIndex" active-color="#ff9854">
+      <template v-for="(item, index) in tabbarData" :key="index">
+        <van-tabbar-item :to="item.path">
+          <span>{{ item.text }}</span>
+          <template #icon>
+            <img
+              :src="
+                getAssetURL(
+                  currentIndex == index ? item.imageActive : item.image
+                )
+              "
+              alt=""
+            />
+          </template>
+        </van-tabbar-item>
+      </template>
+    </van-tabbar>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import tabbarData from "@/assets/data/tabbar.js";
+import { getAssetURL } from "@/utils/load_assets.js";
+
+const currentIndex = ref(0);
+</script>
+
+<style lang="less" scoped>
+.tab-bar {
+  img {
+    height: 24px;
+  }
+}
+</style>
